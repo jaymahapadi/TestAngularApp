909e53b3c3caaf38a71a8cc2d85cbd00d17872ee
Jay Mahapadi <jay.mahapadi@gmail.com>
2025-09-06 21:30:40 +0530

    connection string in program.cs

--- TestAngularApp.Server/Program.cs
+++ TestAngularApp.Server/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("TestData");
+
+// Configure DbContext with the connection string
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
