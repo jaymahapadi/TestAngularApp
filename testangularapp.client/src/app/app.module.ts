61aafcf4c8877f874449d446d162b9885d233e53
Jay Mahapadi <jay.mahapadi@gmail.com>
2025-09-16 18:25:00 +0530

    servie to call api

--- testangularapp.client/src/app/app.module.ts
+++ testangularapp.client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
@@ -17,7 +17,7 @@ import { AddCategoryComponent } from './features/category/add-category/add-categ
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- testangularapp.client/src/app/features/category/add-category/add-category.component.ts
+++ testangularapp.client/src/app/features/category/add-category/add-category.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { FormsModule } from '@angular/forms'; 
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
@@ -6,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css'
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
+  model: AddCategoryRequest;
+
+  private addCategorySubscription? : Subscription;
+
+  constructor(private categoryService:CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: ''
+    };
+  }
+  onFormSubmit() {
+    this.addCategorySubscription= this.categoryService.addCategory(this.model)
+      .subscribe({
+        next: (response) => {
+          console.log("Category added successfully");
+        },
+        error: (error) => {
+          console.log("Error adding category:", error);
+        }
+
+      });
+  }
+
+  
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
+  }
 
 }
